738003acee4bd1ee5a7cab23b72d4441a81a5043
Iago Breno <ibacaraujo@gmail.com>
2019-11-08 09:54:06 -0300

    Create quiz_your_first_join.sql

---
+++ lesson2/quiz_your_first_join.sql
@@ -0,0 +1,9 @@
+SELECT accounts.*, orders.*
+FROM accounts
+JOIN orders
+ON accounts.id = orders.account_id;
+
+SELECT orders.standard_qty, orders.gloss_qty, orders.poster_qty, accounts.website, accounts.primary_poc
+FROM orders
+JOIN accounts
+ON orders.account_id = accounts.id;
