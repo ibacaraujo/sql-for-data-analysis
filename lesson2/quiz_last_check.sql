adfffd8aef013b3e86e5c167fb8779257380bc4c
Iago Breno <ibacaraujo@gmail.com>
2019-11-11 01:29:24 -0300

    Update quiz_last_check.sql

--- lesson2/quiz_last_check.sql
+++ lesson2/quiz_last_check.sql
@@ -5,3 +5,11 @@ ON r.id = s.region_id
 JOIN accounts a
 ON s.id = a.sales_rep_id
 AND r.name = 'Midwest';
+
+SELECT r.name region, s.name sales_rep, a.name account
+FROM region r
+JOIN sales_reps s
+ON r.id = s.region_id
+JOIN accounts a
+ON s.id = a.sales_rep_id
+WHERE r.name = 'Midwest' AND s.name LIKE 'S%'
