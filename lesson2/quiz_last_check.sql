63b14f8738fa05c0f082f60f91d30c68f4929808
Iago Breno <ibacaraujo@gmail.com>
2019-11-12 15:19:55 -0300

    Update quiz_last_check.sql

--- lesson2/quiz_last_check.sql
+++ lesson2/quiz_last_check.sql
@@ -4,7 +4,8 @@ JOIN sales_reps s
 ON r.id = s.region_id
 JOIN accounts a
 ON s.id = a.sales_rep_id
-AND r.name = 'Midwest';
+AND r.name = 'Midwest'
+ORDER BY account;
 
 SELECT r.name region, s.name sales_rep, a.name account
 FROM region r
@@ -22,3 +23,47 @@ JOIN accounts a
 ON s.id = a.sales_rep_id
 WHERE r.name = 'Midwest' AND s.name LIKE '% K%'
 ORDER BY account;
+
+SELECT r.name region, a.name account, (o.total_amt_usd/total+0.01) unit_price
+FROM region r
+JOIN sales_reps s
+ON r.id = s.region_id
+JOIN accounts a
+ON s.id = a.sales_rep_id
+JOIN orders o
+ON a.id = o.account_id
+WHERE o.standard_qty > 100;
+
+SELECT r.name region, a.name account, (o.total_amt_usd/total+0.01) unit_price
+FROM region r
+JOIN sales_reps s
+ON r.id = s.region_id
+JOIN accounts a
+ON s.id = a.sales_rep_id
+JOIN orders o
+ON a.id = o.account_id
+WHERE o.standard_qty > 100 AND o.poster_qty > 50
+ORDER BY unit_price;
+
+SELECT r.name region, a.name account, (o.total_amt_usd/total+0.01) unit_price
+FROM region r
+JOIN sales_reps s
+ON r.id = s.region_id
+JOIN accounts a
+ON s.id = a.sales_rep_id
+JOIN orders o
+ON a.id = o.account_id
+WHERE o.standard_qty > 100 AND o.poster_qty > 50
+ORDER BY unit_price DESC;
+
+SELECT DISTINCT a.name accounts, w.channel
+FROM accounts a
+JOIN web_events w
+ON w.account_id = a.id
+WHERE a.id = 1001;
+
+SELECT o.occurred_at occurred_at, a.name account, o.total total, o.total_amt_usd total_amt_usd
+FROM orders o
+JOIN accounts a
+ON a.id = o.account_id
+WHERE occurred_at BETWEEN '01-01-2015' AND '01-01-2016';
